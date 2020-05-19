a17a234b868132fcd85e7f9cff9d6ddb8296223c
Mahmoud Mansour <mahmodlte@gmail.com>
2020-05-19 06:23:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(e){
+  console.log("DOM has loaded");
+  e = document.getElementById("text");
+  e.innerHTML = "This is really cool!"
+});
+  
